5bde2140d4e2ab511a948161709ed15120f863d7
Dave Eddy <dave@daveeddy.com>
2015-03-28 17:05:00 -0400

    remove unused file, cleanup tests

--- test.js
+++
@@ -1,6 +0,0 @@
-var assert = require('assert');
-var perms = require('./perms');
-
-assert.strictEqual(0755, perms.toMode('rwxr-xr-x'));
-assert.strictEqual(0644, perms.toMode('rw-r--r--'));
-assert.strictEqual(01755, perms.toMode('rwxr-xr-t'));
--- tests/cycle.js
+++ tests/cycle.js
@@ -2,6 +2,6 @@ var perms = require('../');
 var assert = require('assert');
 
 console.log('Cycling every possible mode');
-for (var i = 0; i <= 4095; i++) {
+for (var i = 0; i <= 07777; i++) {
   assert.equal(i, perms.toMode(perms.toString(i)));
 }
